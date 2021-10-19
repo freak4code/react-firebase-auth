13b1510d24df662092d6d714ef7f326d214776ce
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-10-19 20:18:19 +0600

    Add packages and init firebase

---
+++ src/Pages/Authentication/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBU6G6r8-mwC8iVZXCTGM75g2CwcA5Xs2M",
+  authDomain: "snnafi-nodejs-apps.firebaseapp.com",
+  projectId: "snnafi-nodejs-apps",
+  storageBucket: "snnafi-nodejs-apps.appspot.com",
+  messagingSenderId: "818956249792",
+  appId: "1:818956249792:web:0e2d1540ee0a90f6a09b25",
+  measurementId: "G-XX6H4P4MD4",
+};
+
+export default firebaseConfig;
